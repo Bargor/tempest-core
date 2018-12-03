f2e3432cdbb1cae7507c6c52bac176a75c3ab0f4
Karol Kontny <barolek@gmail.com>
2018-12-03 23:10:24 +0100

    Update CMakes for new file names

--- GatherSources.cmake
+++ GatherSources.cmake
@@ -1,12 +1,12 @@
 # This file is part of Tempest-core project
 # Author: Karol Kontny
 
-set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/core)
-set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/assert)
-set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/platform)
+set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/core.h)
+set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/assert.h)
+set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/platform.h)
 set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/macros.h)
 set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/container/queue.h)
-set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/memory/stack_allocator.h)
+set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/memory/frame_allocator.h)
 set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/numeric/random.h)
 set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/time/timer.h)
 set(TEMPEST_CODE ${TEMPEST_CODE} ${CMAKE_SOURCE_DIR}/src/util/variant.h)
