98adf75d5ace48ad55843466031c10355acaec85
Karol Kontny <barolek@gmail.com>
2018-12-17 20:52:40 +0100

    Fixes to perf tests

--- test/perf/queue_test.cpp
+++ test/perf/queue_test.cpp
@@ -1,9 +1,10 @@
-#include <benchmark/benchmark.h>
-#include <queue>
-#include <container/queue.h>
 #include <atomic/spinlock.h>
+#include <benchmark/benchmark.h>
+#include <container/queue.h>
 #include <future>
+#include <queue>
 #include <thread>
+#include <memory>
 
 BENCHMARK_MAIN();
 
@@ -13,21 +14,19 @@ namespace core {
     constexpr std::size_t size = 1023;
 
     static void BM_spsc_queue(benchmark::State& state) {
+        static spsc_queue<std::int32_t, size> queue;
 
-        spsc_queue<std::int32_t, size> queue;
-
-		if (state.thread_index == 0) {
-	        std::int32_t count = 0;
+        if (state.thread_index == 0) {
+            std::int32_t count = 0;
 
             for (auto _ : state) {
-
                 while (count < state.range(0)) {
                     auto res = queue.try_push(count);
                     if (res) ++count;
                 }
             }
-		} else {
-			std::int32_t count = 0;
+        } else {
+            std::int32_t count = 0;
 
             for (auto _ : state) {
                 while (count < state.range(0)) {
@@ -37,41 +36,40 @@ namespace core {
                     }
                 }
             }
-		}
-        
+        }
+
         state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
     }
 
     static void BM_spinlock_queue(benchmark::State& state) {
-        std::queue<std::int32_t> queue;
-        spinlock spin;
+        static std::queue<std::int32_t> queue;
+        static spinlock spin;
+        if (state.thread_index == 0) {
 
-		if (state.thread_index == 0) {
-			std::int32_t count = 0;
-			
-			for (auto _ : state) {
-                while (count < state.range(0)) {
-                    spin.lock();
-                    queue.push(count);
-                    spin.unlock();
-                    ++count;
-                }
-            }
-		} else {
-			std::int32_t count = 0;
+            std::int32_t push_count = 0;
 
             for (auto _ : state) {
-                while (count < state.range(0)) {
-                    if (!queue.empty())
-                    {
+                while (push_count < state.range(0)) {
+                    spin.lock();
+                    queue.push(push_count);
+                    spin.unlock();
+                    ++push_count;
+                }
+            }
+        } else {
+            std::int32_t pop_count = 0;
+
+            for (auto _ : state) {
+                while (pop_count < state.range(0)) {
+                    if (!queue.empty()) {
                         spin.lock();
                         queue.pop();
                         spin.unlock();
-                        ++count;
+                        ++pop_count;
                     }
                 }
             }
-		}
+        }
 
         state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
     }
@@ -79,6 +77,40 @@ namespace core {
     BENCHMARK(BM_spinlock_queue)->RangeMultiplier(2)->Range(size, 8 << 18)->Threads(2);
     BENCHMARK(BM_spsc_queue)->RangeMultiplier(2)->Range(size, 8 << 18)->Threads(2);
 
-}
-}
+    static void BM_spinlock_queue_push(benchmark::State& state) {
+        std::queue<std::int32_t> queue;
+        spinlock spin;
 
+        std::int32_t count = 0;
+
+        for (auto _ : state) {
+            while (count < state.range(0)) {
+                spin.lock();
+                queue.push(count);
+                spin.unlock();
+                ++count;
+            }
+        }
+
+        state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
+    }
+
+    static void BM_spsc_queue_push(benchmark::State& state) {
+        spsc_queue<std::int32_t, 1 << 16> queue;
+
+        std::int32_t count = 0;
+
+        for (auto _ : state) {
+            while (count < state.range(0)) {
+                auto res = queue.try_push(count);
+                if (res) ++count;
+            }
+        }
+
+        state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
+    }
+
+    BENCHMARK(BM_spinlock_queue_push)->RangeMultiplier(2)->Range(size, 1 << 16);
+    BENCHMARK(BM_spsc_queue_push)->RangeMultiplier(2)->Range(size, 1 << 16);
+} // namespace core
+} // namespace tst
