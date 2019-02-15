9f5e685b193adfcf7f8444f161f757ab8b09b595
Karol Kontny <barolek@gmail.com>
2019-02-15 18:07:20 +0100

    Added position

---
+++ src/common/position.h
@@ -0,0 +1,15 @@
+// This file is part of Tempest-core project
+// Author: Karol Kontny
+
+#pragma once
+
+#include <cstdint>
+
+namespace tst {
+
+    template<typename T>
+    struct position {
+        T x;
+        T y;
+    };
+}
