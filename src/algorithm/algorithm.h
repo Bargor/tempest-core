a9e12efad026e82d1415e19fd6dea567f69e741d
Karol Kontny <barolek@gmail.com>
2019-02-19 18:12:51 +0100

    Added for_each_n workaround for GCC

---
+++ src/algorithm/algorithm.h
@@ -0,0 +1,24 @@
+// This file is part of Tempest-core project
+// Author: Karol Kontny
+
+#pragma once
+
+#include <algorithm>
+
+// Workaround for GCC
+
+#if TST_COMPILER & TST_COMPILER_GCC
+
+namespace std {
+template<class InputIt, class Size, class UnaryFunction>
+InputIt for_each_n(InputIt first, Size n, UnaryFunction f) {
+    for (Size i = 0; i < n; ++first, (void)++i) {
+        f(*first);
+    }
+    return first;
+}
+} // namespace std
+
+#endif 
+
+
